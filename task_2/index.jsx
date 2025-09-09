c8233f9bf6c1140e1ab37f2cd914d2cd8890e647
Stanislav Vin <vinninjaq@gmail.com>
2025-09-10 01:09:23 +0200

    feat: second task done (so easy)

--- task_2/index.jsx
+++ task_2/index.jsx
@@ -1,16 +1,21 @@
-import { Fragment, memo } from 'react';
+import { Fragment, memo, useCallback } from 'react'
 
 const MainComponent = () => {
-    const makeLog = () => console.log('hi from MainComponent'); // function to make logs from MainComponent
+  // const makeLog = () => console.log('hi from MainComponent'); // function to make logs from MainComponent
 
-    return (
-        <Fragment>
-            <ChildComponent makeLog={makeLog} />
-        </Fragment>
-    );
-};
+  // обычная практика оптимизации, с которой риходится сталкиваться каждый день
+  const makeLog = useCallback(() => {
+    console.log('hi from MainComponent')
+  }, [])
+
+  return (
+    <Fragment>
+      <ChildComponent makeLog={makeLog} />
+    </Fragment>
+  )
+}
 
 // memoized component
 const ChildComponent = memo(({ makeLog }) => (
-    <button onClick={makeLog}>say Hi from ChildComponent</button>
-));
+  <button onClick={makeLog}>say Hi from ChildComponent</button>
+))
