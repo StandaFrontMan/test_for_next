5e8af8df3c70808a5e359e89486165c152980a12
Stanislav Vin <vinninjaq@gmail.com>
2025-09-10 13:57:04 +0200

    feat: last task

--- task_5/index.tsx
+++ task_5/index.tsx
@@ -1,57 +1,61 @@
-'use client';
+'use client'
 
-import { useState } from 'react';
-import useSWR from 'swr';
+import { fetchOnePost } from '@/libs/fetchOnePost'
+import { useState } from 'react'
+import useSWR from 'swr'
 
-import styles from './page.module.css';
-
-import { fetchOnePost } from '@/libs/fetchOnePost';
+import styles from './page.module.css'
 
 const ComponentOne = () => {
-    const { data } = useSWR('custom_key_1', fetchOnePost);
-    //...some logic
+  const { data } = useSWR('similarKey', fetchOnePost)
+  //...some logic
 
-    return data ? (
-        <div className={styles.card}>
-            <h2>{data.title}</h2>
-            <p>{data.body}</p>
-            <span>ComponentOne</span>
-        </div>
-    ) : (
-        <div>...Loading ComponentOne</div>
-    );
-};
+  return data ? (
+    <div className={styles.card}>
+      <h2>{data.title}</h2>
+      <p>{data.body}</p>
+      <span>ComponentOne</span>
+    </div>
+  ) : (
+    <div>...Loading ComponentOne</div>
+  )
+}
 
 const ComponentTwo = () => {
-    const { data } = useSWR('custom_key_2', () => fetchOnePost({ delayMS: 2000 }));
-    //...some logic
+  const { data } = useSWR('similarKey', () => fetchOnePost({ delayMS: 2000 }))
+  //...some logic
 
-    return data ? (
-        <div className={styles.card}>
-            <h2>{data.title}</h2>
-            <p>{data.body}</p>
-            <span>ComponentTwo</span>
-        </div>
-    ) : (
-        <div>...Loading ComponentTwo</div>
-    );
-};
+  return data ? (
+    <div className={styles.card}>
+      <h2>{data.title}</h2>
+      <p>{data.body}</p>
+      <span>ComponentTwo</span>
+    </div>
+  ) : (
+    <div>...Loading ComponentTwo</div>
+  )
+}
 
 export default function Home() {
-    const [showComponentTwo, setShowComponentTwo] = useState(false);
+  const [showComponentTwo, setShowComponentTwo] = useState(false)
 
-    return (
-        <main className={styles.main}>
-            <div className={styles.description}>
-                <ComponentOne />
-                {showComponentTwo ? (
-                    <ComponentTwo />
-                ) : (
-                    <button className={styles.btn} onClick={() => setShowComponentTwo(true)}>
-                        Show ComponentTwo
-                    </button>
-                )}
-            </div>
-        </main>
-    );
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <ComponentOne />
+        {showComponentTwo ? (
+          <ComponentTwo />
+        ) : (
+          <button
+            className={styles.btn}
+            onClick={() => setShowComponentTwo(true)}
+          >
+            Show ComponentTwo
+          </button>
+        )}
+      </div>
+    </main>
+  )
 }
+
+// нужно использовать один и тот же ключ
